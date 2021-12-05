4e8d284c634da901b45cd19aadac78f4eed1d632
test <test@DESKTOP-OAIJ6CD>
2021-12-04 18:45:19 -0700

    Agregar archivos de proyecto.

---
+++ Proveedores/Dal/ReciboContext.cs
@@ -0,0 +1,21 @@
+﻿using Proveedores.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Proveedores.Dal
+{
+    public class ReciboContext : DbContext
+    {
+        public ReciboContext() : base("ReciboContext")
+        {
+        }
+
+        public DbSet<recibos> Students { get; set; }
+     
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ Proveedores/Models/recibos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proveedores.Models
+{
+    public class recibos
+    {
+        public int ID { get; set; }
+        public string Proveedor { get; set; }
+        public int Monto { get; set; }
+        public int Moneda { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Comentario { get; set; }
+
+    }
+}
